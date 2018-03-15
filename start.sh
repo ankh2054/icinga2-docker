286f53e5a6e9073cf79d19c4c4175849a2c94dbe
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-03-15 11:40:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 
+# Testing
+tail -f /dev/null
 # Start Supervisor 
-echo "Starting Supervisor"
-/usr/bin/supervisord -n -c /etc/supervisord.conf
+#echo "Starting Supervisor"
+#/usr/bin/supervisord -n -c /etc/supervisord.conf
